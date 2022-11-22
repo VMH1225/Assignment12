ca8821720020b202d7da286631ea3be9e42c88b1
VMH1225 <huertavictor1225@gmail.com>
2022-11-21 19:03:30 -0800

    challenge 1 complete. array allows for integer values from 1-100

--- main.cpp
+++ main.cpp
@@ -15,12 +15,13 @@ void challenge1();
 void challenge2();
 void challenge3();
 int* createRandomArray(int);
+void addElement(int&);
+void printArray(int);
+void serialSearch(int, int);
 int* ptr = nullptr;
 
 int main() {
 
-		// move ptr from challenge1 and create the ptr here that is visible for the first 2 challenges and release the
-		// dynamically allocated memory after each program uses it. 
 	do {
 		switch (menuOption()) {
 		case 0: exit(0);
@@ -49,7 +50,7 @@ int menuOption() {
 void challenge1() {	
 	int option;
 	bool flag = true;
-	
+	int sizeOfArray = 0;
 	do {
 		cout << "\n\t1> Searching unsorted dynamic arrays\n\t";
 		cout << string(75, 205);
@@ -61,7 +62,7 @@ void challenge1() {
 		cout << string(75, 196);
 		cout << "\n\t0> return\n\t";
 		cout << string(75, 205);
-		option = inputInteger("\n\tOption: ", 0, 4);
+		option = inputInteger("\n\tOption: ", 0, 5);
 		flag = true;
 		switch (option) {
 		case 0: {
@@ -73,13 +74,36 @@ void challenge1() {
 		case 1: {
 			delete[] ptr;
 			system("cls"); 
-			int num = inputInteger("\n\tEnter the size of the dynamic array: ",1, 10000);
-			ptr = createRandomArray(num);
+			sizeOfArray = inputInteger("\n\tEnter the size of the dynamic array: ",1, 10000);
+			ptr = createRandomArray(sizeOfArray);
 			system("pause");
 			break;
 		}
 		case 2: {
-			cout << "\nhi";
+			addElement(sizeOfArray);
+			break;
+		}
+		case 3: {
+			printArray(sizeOfArray);
+			break;
+		}
+		case 4: {
+			int searchValue = inputInteger("\n\tEnter a value to search for in the array: ", 0, 100);
+			
+			serialSearch(searchValue, sizeOfArray);
+			break;
+		}
+		case 5:{
+			if (sizeOfArray > 0) {
+				delete[] ptr;
+				ptr = nullptr;
+				cout << "\n\tElements from the array have been cleared/deleted.";
+			}
+			else {
+				cout << "\n\tThe array is empty.";
+			}
+			sizeOfArray = 0;
+		    break;
 		}
 		default: break;
 		}
@@ -105,6 +129,61 @@ int* createRandomArray(int size) {
 	return arrPtr;
 }
 
+void addElement(int &size) {
+	int num;
+	if (ptr == nullptr) {
+		num = inputInteger("\n\tEnter a number to add to the dynamic array: ", 0, 100);
+		ptr = new int[1];
+		size = 1;
+	}
+	else {
+		size += 1;
+		int* temp = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			temp[i] = ptr[i];
+		}	
+		num = inputInteger("\n\tEnter a number to add to the dynamic array: ", 0, 100);
+		temp[size - 1] = num;
+
+		delete[] ptr;
+		ptr = temp;
+		temp = nullptr;
+	}
+}
+
+void printArray(int size) {
+	if (size == 0) {
+		cout << "\n\tThe array is empty.";
+	}
+	else {
+		cout << "\n\tArray values:";
+		for (int i = 0; i < size; i++) {
+		      cout << "\n\t" << ptr[i];
+		}
+	}
+}
+
+void serialSearch(int val, int size) {
+	int index = -1;
+	if (size == 0) {
+		cout << "\n\tThe array is empty.";
+		return;
+	}
+	for (int i = 0; i < size; i++) {
+		if (ptr[i] == val) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index == -1) {
+		cout << "\n\tCould not locate " << val << " in array.";
+	}
+	else {
+		cout << "\n\tValue " << val << " found at index " << index << ".";
+	}
+}
 void challenge2() {
 	cout << "\n\t2> Searching sorted dynamic arrays\n\t";
 	cout << string(75, 205);
