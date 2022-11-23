72bd3556dd558505a4f89706ec2ca13e5936f7fc
VMH1225 <huertavictor1225@gmail.com>
2022-11-22 19:18:53 -0800

    challenge 2 initial commit. added selection sort but still using serial search

--- main.cpp
+++ main.cpp
@@ -19,6 +19,10 @@ string* createRandomArray(int);
 void addElement(int&);
 void printArray(int);
 void serialSearch(string, int);
+void selectionSort(string*, int);
+int binarySearchString(string* arr, string x, int n);
+bool is_number(const std::string& s);
+void selectionSort(string* array, int NUM_NAMES);
 string* ptr = nullptr;
 
 int main() {
@@ -33,6 +37,7 @@ int main() {
 	} while (true);
 }
 
+// add selection sort function and swap function for challenge 2
 int menuOption() {	
 
 	cout << "\n\tCMPR131 Chapter 12: Searches and hashing by Victor Huerta (11/22/22)\n\t";
@@ -186,18 +191,111 @@ void serialSearch(string val, int size) {
 	}
 }
 void challenge2() {
-	cout << "\n\t2> Searching sorted dynamic arrays\n\t";
-	cout << string(75, 205);
-	cout << "\n\t1> Read data file and store into the array";
-	cout << "\n\t2> Add an element to the dynamic array";
-	cout << "\n\t3> Display elements from the array";
-	cout << "\n\t4> Search for an element from the array";
-	cout << "\n\t5> Clear the array\n\t";
-	cout << string(75, 196);
-	cout << "\n\t0> return\n\t";
-	cout << string(75, 205);
-	int option = inputInteger("\n\tOption: ", 0, 4);
+	int option;
+	bool flag = true;
+	int sizeOfArray = 0;
+	do {
+		cout << "\n\t2> Searching sorted dynamic arrays\n\t";
+		cout << string(75, 205);
+		cout << "\n\t1> Read data file and store into the array";
+		cout << "\n\t2> Add an element to the dynamic array";
+		cout << "\n\t3> Display elements from the array";
+		cout << "\n\t4> Search for an element from the array";
+		cout << "\n\t5> Clear the array\n\t";
+		cout << string(75, 196);
+		cout << "\n\t0> return\n\t";
+		cout << string(75, 205);
+		int option = inputInteger("\n\tOption: ", 0, 5);
+		flag = true;
+		switch (option) {
+		case 0: {
+			delete[] ptr;
+			ptr = nullptr;
+			flag = false;
+			break;
+		}
+		case 1: {
+			delete[] ptr;
+			system("cls");
+			sizeOfArray = inputInteger("\n\tEnter the size of the dynamic array: ", 1, 10000);
+			ptr = createRandomArray(sizeOfArray);
+			selectionSort(ptr,sizeOfArray);
+			system("pause");
+			break;
+		}
+		case 2: {
+			addElement(sizeOfArray);
+			selectionSort(ptr, sizeOfArray);
+			break;
+		}
+		case 3: {
+			printArray(sizeOfArray);
+			break;
+		}
+		case 4: {
+			string searchValue = inputString("\n\tEnter a value to search for in the array: ", false);
+			if (sizeOfArray > 0) {
+				serialSearch(searchValue, sizeOfArray);
+			}
+			else {
+				cout << "\n\tThe array is empty";
+			}
+			break;
+		}
+		case 5: {
+			if (sizeOfArray > 0) {
+				delete[] ptr;
+				ptr = nullptr;
+				cout << "\n\tElements from the array have been cleared/deleted.";
+			}
+			else {
+				cout << "\n\tThe array is empty.";
+			}
+			sizeOfArray = 0;
+			break;
+		}
+		default: break;
+		}
+	} while (flag);
 }
+
+
+bool is_number(const std::string& s)
+{
+	return !s.empty() && std::find_if(s.begin(),
+		s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
+
+void selectionSort(string* array, int NUM_NAMES)
+{
+	int startScan, minIndex;
+	string minValue;
+
+	for (startScan = 0; startScan < (NUM_NAMES - 1); startScan++)
+	{
+
+		minIndex = startScan;
+		minValue = array[startScan];
+		for (int index = startScan + 1; index < NUM_NAMES; index++)
+		{
+			if (is_number(array[index]) && is_number(minValue)) {
+				if (stoi(array[index]) < stoi(minValue))
+				{
+					minValue = array[index];
+					minIndex = index;
+				}
+			}
+			else
+			{
+				minValue = array[index];
+				minIndex = index;
+			}
+		}
+		array[minIndex] = array[startScan];
+		array[startScan] = minValue;
+	}
+}
+
 
 void challenge3() {
 	cout << "\n\t3> Application using hashing\n\t";
